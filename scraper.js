db9dc3b2e67814752cced236619a187f51e4c9ba
Edwin Alejandro Rodriguez Zubieta <edwinzubieta2401@gmail.com>
2026-03-26 21:57:31 -0500

    api con camuflaje

--- scraper.js
+++ scraper.js
@@ -5,7 +5,17 @@ const TurndownService = require('turndown');
 
 async function extraerTextoLimpio(url) {
     try {
-        const respuesta = await axios.get(url);
+        const respuesta = await axios.get(url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+                'Accept-Language': 'es-CO,es;q=0.9,en-US;q=0.8,en;q=0.7',
+                'Cache-Control': 'no-cache',
+                'Pragma': 'no-cache',
+            },
+            // Le damos un máximo de 15 segundos para responder, si no, aborta rápido
+            timeout: 15000 
+        });
         const $ = cheerio.load(respuesta.data);
 
         // Limpieza de ruido
